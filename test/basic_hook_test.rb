16cf54742b509dd636b174279d74fe7928ac7476
Noah Gibbs <noah@onlive.com>
2013-04-19 09:25:01 -0700

    Start building test infrastructure

---
+++ test/basic_hook_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+require "fileutils"
+require "minitest/autorun"
+
+class BasicHookTest < MiniTest::Unit::TestCase
+  REPOS_DIR = File.join(File.dirname(__FILE__), "repos")
+
+  def setup
+    # Empty out the test repos dir
+    FileUtils.rm_rf File.join(REPOS_DIR, "*")
+
+    # Create local parent and child repos with a single shared commit
+    Dir.chdir REPOS_DIR do
+      Hook.shell! "mkdir parent_repo.git && cd parent_repo.git && git init --bare"
+      Hook.shell! "git clone parent_repo.git child_repo"
+      Hook.shell! "cd child_repo && echo Bob > README && git add README && git commit -m 'README' && git push"
+    end
+  end
+
+  def test_simple_pre_commit
+    assert true
+  end
+
+  def test_simple_pre_receive
+    assert true
+  end
+
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,7 @@
+# TODO: coverage?
+
+# Test local copy first
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
+require "minitest"
+require "minitest/pride"
