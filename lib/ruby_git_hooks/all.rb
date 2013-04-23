802cbd47a350126a1a49b273ead24342e8bff70c
Noah Gibbs <noah@onlive.com>
2013-04-23 12:09:20 -0700

    Add ruby_debug hook, mostly for testing. VAL-105

--- lib/ruby_git_hooks/all.rb
+++ lib/ruby_git_hooks/all.rb
@@ -1,3 +1,5 @@
 require "ruby_git_hooks"
 require "ruby_git_hooks/case_clash"
 require "ruby_git_hooks/max_file_size"
+require "ruby_git_hooks/copyright_check"
+require "ruby_git_hooks/ruby_debug_hook"
---
+++ lib/ruby_git_hooks/ruby_debug.rb
@@ -0,0 +1,22 @@
+require "ruby_git_hooks"
+
+# This exists primarily for testing.  I mean, you can use it and all, but...
+
+class RubyDebugHook < RubyGitHooks::Hook
+  def check
+    bad_files = []
+
+    file_diffs.each do |file, diff|
+      if diff.include? "require 'ruby-debug'"
+        bad_files << file
+      end
+    end
+
+    return true if bad_files.empty?
+
+    puts "You left requires of ruby-debug in the following files:\n"
+    puts bad_files.join("\n")
+
+    false
+  end
+end
