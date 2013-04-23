eeed9efdf876c709ed15c470a6bf5b95a7a10bf0
Noah Gibbs <noah@onlive.com>
2013-04-23 09:36:54 -0700

    More Copyright Check tests

--- test/copyright_check_test.rb
+++ test/copyright_check_test.rb
@@ -7,7 +7,7 @@ class CopyrightCheckHookTest < HookTestCase
   REPOS_DIR = File.expand_path File.join(File.dirname(__FILE__), "repos")
   FAKE_MAILER = File.join(File.dirname(__FILE__), "fake_mailer")
   MAILER_FILE = File.join(File.dirname(__FILE__), "mail_params")
-  TEST_HOOK_BODY_1 = <<TEST
+  TEST_HOOK_BASIC = <<TEST
 #!/usr/bin/env ruby
 require "ruby_git_hooks/copyright_check"
 
@@ -21,6 +21,13 @@ RubyGitHooks.register CopyrightCheckHook.new("domain" => "onlive.com",
 )
 
 RubyGitHooks.run
+TEST
+
+  TEST_HOOK_NO_SEND = <<TEST
+#!/usr/bin/env ruby
+require "ruby_git_hooks/copyright_check"
+
+RubyGitHooks.run CopyrightCheckHook.new("no_send" => true)
 TEST
 
   def setup
@@ -40,7 +47,7 @@ TEST
   end
 
   def test_copyright_post_commit
-    add_hook("child_repo", "post-commit", TEST_HOOK_BODY_1)
+    add_hook("child_repo", "post-commit", TEST_HOOK_BASIC)
 
     new_commit("child_repo", "myfile.rb", <<FILE_CONTENTS)
 # No copyright notice!
@@ -56,11 +63,32 @@ FILE_CONTENTS
       "Mail message must mention myfile.rb!"
   end
 
-  def test_copyright_no_problem
-    add_hook("child_repo", "post-commit", TEST_HOOK_BODY_1)
+  def test_copyright_no_send_option
+    add_hook("child_repo", "post-commit", TEST_HOOK_NO_SEND)
+
+    new_commit("child_repo", "myfile.rb", <<FILE_CONTENTS)
+# No copyright notice!
+FILE_CONTENTS
+
+    assert !File.exist?(MAILER_FILE),
+      "Copyright test must not send email if 'no_send' is set!"
+  end
+
+  def test_copyright_basic_correct
+    add_hook("child_repo", "post-commit", TEST_HOOK_BASIC)
 
     new_commit("child_repo", "myfile.rb", <<FILE_CONTENTS)
 # Copyright (C) 1941-2013 YoyoDyne, Inc.  All Rights Reserved.
+FILE_CONTENTS
+
+    assert !File.exist?(MAILER_FILE), "Copyright test must not send email!"
+  end
+
+  def test_copyright_no_first_year
+    add_hook("child_repo", "post-commit", TEST_HOOK_BASIC)
+
+    new_commit("child_repo", "myfile.rb", <<FILE_CONTENTS)
+# Copyright (C) 2013 YoyoDyne, Inc.  All Rights Reserved.
 FILE_CONTENTS
 
     assert !File.exist?(MAILER_FILE), "Copyright test must not send email!"
