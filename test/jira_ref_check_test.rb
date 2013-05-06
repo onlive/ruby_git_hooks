0a09600b22817b3c1fa63ae06b1071180cb2b102
Ruth Helfinstein <ruth.helfinstein@onlive.com>
2013-05-06 14:04:51 -0700

    Add test for multiple JIRA ticket references. VAL-107

--- test/jira_ref_check_test.rb
+++ test/jira_ref_check_test.rb
@@ -42,4 +42,35 @@ JSON
     assert_equal true, @hook.check
   end
 
+  def test_multiple_references_with_good
+    mock(@hook).commit_message.at_least(1) { "Message with CLOSE-123 BAD-456 GOOD-234 NOT-123 reference to Jira" }
+    mock(RestClient).get("https://user:password@jira.onlive.com/rest/api/latest/issue/GOOD-234") { <<JSON }
+{ "fields": { "status": { "name": "Open" } } }
+JSON
+    mock(RestClient).get("https://user:password@jira.onlive.com/rest/api/latest/issue/BAD-456") do
+      exc = RestClient::Exception.new
+      mock(exc).http_code.at_least(1) { 404 }
+      raise exc
+    end
+    mock(RestClient).get("https://user:password@jira.onlive.com/rest/api/latest/issue/CLOSE-123") { <<JSON }
+{ "fields": { "status": { "name": "Closed" } } }
+JSON
+    assert_equal true, @hook.check
+
+  end
+  
+  def test_multiple_references_none_good
+    mock(@hook).commit_message.at_least(1) { "Message with CLOSE-123 BAD-456 reference to Jira" }
+    mock(RestClient).get("https://user:password@jira.onlive.com/rest/api/latest/issue/BAD-456") do
+      exc = RestClient::Exception.new
+      mock(exc).http_code.at_least(1) { 404 }
+      raise exc
+    end
+    mock(RestClient).get("https://user:password@jira.onlive.com/rest/api/latest/issue/CLOSE-123") { <<JSON }
+{ "fields": { "status": { "name": "Closed" } } }
+JSON
+    assert_equal false, @hook.check
+
+  end
+
 end
